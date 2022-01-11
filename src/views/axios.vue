82f5f4b6b2e26c0085d7ee66388d8f1a7b1b70f6
cggcbb <449567533@qq.com>
2022-01-11 11:40:58 +0800

    update axios

---
+++ src/views/axios.vue
@@ -0,0 +1,21 @@
+<template lang="">
+    <n-button @click="send" :loading="loading">send http to github</n-button>
+</template>
+<script lang="ts" setup>
+import http from '../utils/http'
+import { useMessage } from 'naive-ui'
+import { ref } from 'vue'
+
+const message = useMessage()
+const loading = ref<Boolean>(false)
+
+const send = async () => {
+    loading.value = true
+    const result = await http.get('https://api.github.com/users/cggcbb')
+    loading.value = false
+    message.success(`pull github success, username: ${result.data.login}`, {
+        duration: 5000
+    })
+}
+</script>
+<style lang=""></style>
